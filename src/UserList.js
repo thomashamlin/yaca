6c0f041be855b18a7e399bbec152df2211a35290
Tom Hamlin <tom@metaphorlab.com>
2018-05-23 20:59:08 -0700

    App root contains UserList
    Add Doug Crockford for good luck.

--- src/UserList.js
+++ src/UserList.js
@@ -1,35 +1,19 @@
 import React from 'react';
-import { fetchJson } from './dataSources';
 
 
 /**
  * Renders a list of users.
- *
- * @param {Object[]} users: [{id: 3, name: 'Sam'},...]
  */
-class UserList extends React.Component {
-  constructor() {
-    super();
-    this.state = { users: [] };
-  }
-
-  componentDidMount() {
-    fetchJson('/data/users.json', (data) => {
-      this.setState({ users: data });
-    });
-  }
-
-  render() {
-    return (
-      <ul className="users">
-        {this.state.users.map(user => (
-          <li key={user.id}>
-            {user.name}
-          </li>
-        ))}
-      </ul>
-    );
-  }
+function UserList({ users }) {
+  return (
+    <ul className="users">
+      {users.map(user => (
+        <li key={user.id}>
+          {user.name}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default UserList;
--- src/app.js
+++ src/app.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UserList from './UserList';
+import { fetchJson } from './dataSources';
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    // Initial state
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    fetchJson('/data/users.json', (data) => {
+      this.setState({ users: data });
+    });
+  }
+
+  render() {
+    return (
+      <div id="container">
+        <aside id="users" className="column">
+          <UserList users={this.state.users} />
+        </aside>
+        <main className="column">
+          <section id="messages">
+          </section>
+          <section id="chatForm">
+            <form>
+              <input type="text" name="message" />
+              <button type="submit">Send</button>
+            </form>
+          </section>
+        </main>
+      </div>
+    );
+  }
+}
 
 
 ReactDOM.render(
-  <UserList />,
+  <App />,
   document.getElementById('app')
 );
